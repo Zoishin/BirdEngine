c0b47bcfca40e34847b9de6c9e0bac5541c77cbe
Zoishin <zwc23123@163.com>
2023-02-18 20:40:49 +0800

    Support premake

--- premake5.lua
+++ premake5.lua
@@ -1,206 +1,117 @@
-workspace "BirdEngine" 
-
-	architecture "x64"
-
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
-
-outputdir="%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-project "BirdEngine"
-	location "BirdEngine"
-	kind "SharedLib"
-	language "C++"
-
-	targetdir("bin/" .. outputdir .. "/%{prj.name}")
-	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-
-
-	files
-
-	{
-
-		"%{prj.name}/src/**.h",
-
-		"%{prj.name}/src/**.cpp",
-
-		"%{prj.name}/src/**.hpp"
-
-	}
-
-
-
-	includedirs
-
-	{
-
-		"%{prj.name}/vendor/spdlog/include",
-
-		"%{prj.name}/src/Hazel/vendor/spdlog/include"
-
-	}
-
-
-
-	filter "system:windows"
-
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-
-		{
-
-			"SPDLOG_COMPILED_LIB",
-
-			"BE_PLATFORM_WINDOWS",
-
-			"BE_BUILD_DLL",
-
-			"_WINDLL"
-
-		}
-
-
-
-		postbuildcommands
-
-		{
-
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/".. outputdir .."/SandBox")
-
-		}
-
-
-
-	filter "configurations:Debug"
-
-		defines "HZ_DEBUG"
-
-		symbols "On"
-
-	
-
-
-
-	filter "configurations:Release"
-
-		defines "HZ_RELEASE"
-
-		optimize "On"
-
-
-
-
-
-	filter "configurations:Dist"
-
-		defines "HZ_DIST"
-
-		optimize "On"
-
-
-
-project "SandBox"
-
-	location "SandBox"
-
-	kind "ConsoleApp"
-
-	language "C++"
-
-
-
-	targetdir("bin/" .. outputdir .. "/%{prj.name}")
-
-	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-
-
-	files
-
-	{
-
-		"%{prj.name}/src/**.h",
-
-		"%{prj.name}/src/**.cpp",
-
-		"%{prj.name}/src/**.hpp"
-
-	}
-
-
-
-	includedirs
-
-	{
-
-		"Hazel/src/Hazel/vendor/spdlog/include",
-
-		"Hazel/src" 
-
-	}
-
-
-
-	links
-
-	{
-
-		"Hazel"
-
-	}
-
-
-
-	filter "system:windows"
-
-		cppdialect "C++17"
-
-		staticruntime "On"
-
-		systemversion "latest"
-
-
-
-		defines
-
-		{
-
-			"HZ_PLATFORM_WINDOWS",
-
-		}
-
-
-
-	filter "configurations:Debug"
-
-		defines "HZ_DEBUG"
-
-		symbols "On"
-
-	
-
-
-
-	filter "configurations:Release"
-
-		defines "HZ_RELEASE"
-
-		optimize "On"
-
-
-
-
-
-	filter "configurations:Dist"
-
-		defines "HZ_DIST"
-
-		optimize "On"
+workspace "BirdEngine" --解决方案名称
+    architecture "x86_64" --编译平台 只编64位--(x86,x86_64,ARM)
+	startproject "Sandbox"
+    configurations 
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+--临时变量 定义 输出目录
+--详细的所有支持的tokens 可参考 [https://github.com/premake/premake-core/wiki/Tokens]
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "BirdEngine" --项目名称
+    location "BirdEngine" --相对路径
+    kind "SharedLib" --表明该项目是dll动态库
+    language "c++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")--输出目录
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")--中间临时文件的目录
+
+    files--该项目的文件
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs--附加包含目录
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"--windows平台的配置
+        cppdialect "c++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines --预编译宏
+        {
+            "BE_BUILD_DLL",
+            "BE_PLATFORM_WINDOWS",
+            "_WINDLL",
+            "_UNICODE",
+            "UNICODE",
+        }
+
+        postbuildcommands -- build后的自定义命令
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox") --拷贝引擎dll库到sanbox.exe的同一目录下去
+        }
+
+    filter "configurations:Debug"
+        defines "BE_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "BE_RELEASE"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        defines "BE_DIST"
+        runtime "Release"
+        optimize "on"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "c++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "BirdEngine/vendor/spdlog/include",
+        "BirdEngine/src"
+    }
+
+    links
+    {
+        "BirdEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "c++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "BE_PLATFORM_WINDOWS",
+            "_UNICODE",
+            "UNICODE",
+        }
+
+    filter "configurations:Debug"
+        defines "BE_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "BE_RELEASE"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        defines "BE_DIST"
+        runtime "Release"
+        optimize "on"
